f9ac8264fc85bcdac46caef10abe776c51dfbe0e
Stephen Okwiri <85963471+sokwiri@users.noreply.github.com>
2025-07-03 14:30:34 +0300

    errors fixed

--- fruit-service-engine/src/main/java/server/tasks/AddFruitPrice.java
+++ fruit-service-engine/src/main/java/server/tasks/AddFruitPrice.java
@@ -8,6 +8,10 @@ import model.FruitPrice;
 import server.FruitComputeEngine;
 import server.interfaces.Task;
 
+/**
+ * Task to add a fruit price to the compute engine.
+ */
+
 public class AddFruitPrice implements Task<String> {
     private FruitPrice fruitPrice;
 
@@ -18,9 +22,10 @@ public class AddFruitPrice implements Task<String> {
     @Override
     public String execute() throws RemoteException {
         try {
-            // Get the engine instance and add the fruit price to its storage
+            // Connect to the RMI registry and get the FruitComputeEngine instance
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
             FruitComputeEngine engine = (FruitComputeEngine) registry.lookup("FruitComputeEngine");
+            // Add the fruit price to the server's storage
             engine.addFruitPrice(fruitPrice.getFruitName(), fruitPrice.getPrice());
             
             System.out.println("Adding fruit price: " + fruitPrice.getFruitName() + " - $" + fruitPrice.getPrice());
